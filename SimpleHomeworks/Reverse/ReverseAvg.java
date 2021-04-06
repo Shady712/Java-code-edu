95bae477fa9a38e27f02f32fcaab73f0e0afe992
Krainov Danil <frostmorn712@gmail.com>
2021-04-07 00:07:16 +0300

    Codestyle fix

--- SimpleHomeworks/Reverse/ReverseAvg.java
+++ SimpleHomeworks/Reverse/ReverseAvg.java
@@ -6,40 +6,40 @@ import java.util.List;
 public class ReverseAvg {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-	List<int[]> array = new ArrayList<>();
-	int maxLen = 0;
-	while (input.hasNextLine()) {
-	    Scanner tmp = new Scanner(input.nextLine());
-	    int k = 0;
-	    int[] cur = new int[1];
-	    while (tmp.hasNextInt()) {
-		if (k == cur.length) {
-		    cur = Arrays.copyOf(cur, cur.length * 2);
+		List<int[]> array = new ArrayList<>();
+		int maxLen = 0;
+		while (input.hasNextLine()) {
+			Scanner tmp = new Scanner(input.nextLine());
+			int k = 0;
+			int[] cur = new int[1];
+			while (tmp.hasNextInt()) {
+			if (k == cur.length) {
+				cur = Arrays.copyOf(cur, cur.length * 2);
+			}
+				cur[k++] = tmp.nextInt();
+			}
+			cur = Arrays.copyOf(cur, k);
+			if (maxLen < k) {
+				maxLen = k;
+			}
+			array.add(cur);
 		}
-	    	cur[k++] = tmp.nextInt();
-	    }
-	    cur = Arrays.copyOf(cur, k);
-	    if (maxLen < k) {
-	        maxLen = k;
-	    }
-	    array.add(cur);
-    }
-	long[] lines = new long[array.size()];
-	long[] columns = new long[maxLen];
-	int[] number = new int[maxLen];
-	for (int i = 0; i < array.size(); i++) {
-	    for (int j = 0; j < array.get(i).length; j++) {
-		lines[i] += array.get(i)[j];
-	    	number[j]++;
-		columns[j] += array.get(i)[j];
+		long[] lines = new long[array.size()];
+		long[] columns = new long[maxLen];
+		int[] number = new int[maxLen];
+		for (int i = 0; i < array.size(); i++) {
+			for (int j = 0; j < array.get(i).length; j++) {
+				lines[i] += array.get(i)[j];
+				number[j]++;
+				columns[j] += array.get(i)[j];
             }
-	}
+		}
 	
-	for (int i = 0; i < array.size(); i++) {
-	    for (int j = 0; j < array.get(i).length; j++) {
-	        System.out.print((lines[i] + columns[j] - array.get(i)[j]) / (array.get(i).length + number[j] - 1) + " ");
-	    }
-	    System.out.println();
-	}
+		for (int i = 0; i < array.size(); i++) {
+			for (int j = 0; j < array.get(i).length; j++) {
+				System.out.print((lines[i] + columns[j] - array.get(i)[j]) / (array.get(i).length + number[j] - 1) + " ");
+			}
+			System.out.println();
+		}
     }
 }
